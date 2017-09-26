158bb337a0b657d5f01a2c82f98d09d285eb8673
Christian Mäder <mail@cimnine.ch>
2017-09-26 23:59:22 +0200

    Updated for reception 2.0

--- Formula/reception.rb
+++ Formula/reception.rb
@@ -1,32 +1,31 @@
 class Reception < Formula
   desc "Entry page & reverse proxy for all your docker-compose projects."
   homepage "https://github.com/ninech/reception"
-  url "https://github.com/ninech/reception/archive/v0.2.0.tar.gz"
-  sha256 "2b2baf13bee061b1b91a08f44234a5fc22a1f393b2d35a51e3b3edb96b4aad99"
+  url "https://github.com/ninech/reception/archive/2.0.0.tar.gz"
+  version "2.0.0"
+  sha256 "35c8a221350b684906983286bc7423b79b6a469fce2abfe9a797ffb264ddca3d"
   head "https://github.com/ninech/reception.git"
 
-  depends_on "docker-gen" => :run
-  depends_on "dnsmasq" => :run
-  depends_on "nginx" => :run
+  depends_on "go" => :build
   depends_on "docker" => :optional
   depends_on "docker-compose" => :optional
 
   def install
-    etc_reception = (HOMEBREW_PREFIX+"etc/reception")
-    etc_reception.mkpath
-    etc_reception.install "docker-gen.osx.conf",
-                          "index.html.tmpl",
-                          "nginx.conf.tmpl"
-    prefix.install Dir["*"]
+    mkdir_p buildpath/"src/github.com/ninech"
+    ln_sf buildpath, buildpath/"src/github.com/ninech/reception"
+
+    system "make", "build"
+
+    bin.install "reception"
   end
 
   def caveats
     <<-EOS.undent
-      Read https://github.com/ninech/reception#macos to learn how to complete your setup!
+      Read https://github.com/ninech/reception#macos to learn how to complete the setup!
     EOS
   end
 
-  plist_options :manual => "cd /usr/local/etc/reception; sudo docker-gen -config #{HOMEBREW_PREFIX}/etc/reception/docker-gen.osx.conf"
+  plist_options :manual => "sudo reception"
 
   def plist; <<-EOS.undent
     <?xml version="1.0" encoding="UTF-8"?>
@@ -39,14 +38,12 @@ class Reception < Formula
       <string>ch.nine.reception</string>
       <key>ProgramArguments</key>
       <array>
-        <string>#{HOMEBREW_PREFIX}/bin/docker-gen</string>
-        <string>-config</string>
-        <string>#{etc}/reception/docker-gen.osx.conf</string>
+        <string>#{HOMEBREW_PREFIX}/bin/reception</string>
       </array>
       <key>RunAtLoad</key>
       <true/>
       <key>WorkingDirectory</key>
-      <string>#{etc}/reception</string>
+      <string>#{var}/run/</string>
       <key>StandardErrorPath</key>
       <string>#{var}/log/reception.log</string>
       <key>StandardOutPath</key>
@@ -57,10 +54,6 @@ class Reception < Formula
   end
 
   test do
-    # This util is merely a bunch of pre-defined config files
-    # and the main util `docker-gen` has no config test option.
-    # That's why I really can't come up with a test that would
-    # test anything meaningfull.
-    system "true"
+    system bin/"reception", "-v"
   end
 end
